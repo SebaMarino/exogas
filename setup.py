dd514f2439342cd33b46d705369427ff25545816
SebaMarino <sebastian.marino.estay@gmail.com>
2022-02-11 12:26:46 +0000

    updated

--- setup.py
+++ setup.py
@@ -2,18 +2,21 @@ from setuptools import setup
 setup(
     name = 'exogas',         # How you named your package folder (MyLib)
     packages = ['exogas'],   # Chose the same as "name"
-    version = '1.1',      # Start with a small number and increase it with every change you make
+    version = '1.0.1',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description = 'Exogas is a package to simulate the evolution of exocometary gas in debris discs.',   # Give a short description about your library
     author = 'Sebastian Marino',                   # Type in your name
     author_email = 'sebastian.marino.estay@gmail.com',      # Type in your E-Mail
     url = 'https://github.com/SebaMarino/exogas',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/SebaMarino/exogas/archive/v0.4.1.tar.gz',    #
+    download_url = 'https://github.com/SebaMarino/exogas/archive/refs/tags/v1.0.1.tar.gz',    #
     keywords = ['gas', 'exocomet', 'viscous', 'photodissociation'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
+    install_requires=[
         'numpy',
         'matplotlib',
-        'scipy'
+        'scipy',
+        'tqdm',
+        'sys',
+        'os',
     ],
     include_package_data=True,
     # package_data={'exogas':['photodissociation/*.txt'],},
